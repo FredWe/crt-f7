7a40ea70094d087fbec27a65158a3fa0e9982541
Fred.W <fred.wei@foxmail.com>
2015-07-28 11:01:00 +0800

    no update

--- push.ps1
+++ push.ps1
@@ -1,8 +1,4 @@
-cd ../crt
-$m = read-host "Please enter message for 'crt'"
-git commit -a -m $m
-git push origin master
-cd ../CRT-f7
 $m = read-host "Please enter message for 'CRT-f7'"
+git add .
 git commit -a -m $m
 git push origin master
