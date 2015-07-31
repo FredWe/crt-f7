8e71c4897f15f5342c55b888906db095f42d11b1
Fred.W <fred.wei@foxmail.com>
2015-07-31 14:52:58 +0800

    bug fixed

--- push.ps1
+++ push.ps1
@@ -1,4 +1,13 @@
 $m = read-host "Please enter message for 'CRT-f7'"
+
+git checkout gh-pages
 git add .
 git commit -a -m $m
-git push origin gh-pages 
+git push origin gh-pages
+
+git checkout master
+git add .
+git commit -a -m $m
+git push origin master
+
+git checkout gh-pages
