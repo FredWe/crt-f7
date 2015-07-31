50e7103ea4c392d0053cd8591142b1a19787d165
Fred.W <fred.wei@foxmail.com>
2015-07-31 14:45:14 +0800

    gh-pages without Font-Awesome/src

--- push.ps1
+++ push.ps1
@@ -1,4 +1,8 @@
 $m = read-host "Please enter message for 'CRT-f7'"
+git checkout gh-pages
 git add .
 git commit -a -m $m
-git push origin gh-pages 
+git push origin gh-pages
+git checkout master
+git push origin master
+git checkout gh-pages
